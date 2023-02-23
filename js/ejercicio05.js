137a2f2b1199ef1a189236a2d586cd894f8364d3
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-02-23 10:50:45 -0300

    Ejercicio 5 Realizado

---
+++ js/ejercicio05.js
@@ -0,0 +1,7 @@
+/*
+5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+*/
+
+let numero1 = parseInt(window.prompt("Introduza un número: ")); 
+let numero2 = parseInt(window.prompt("Introduza otro número: ")); 
+document.write("El resultado de sumar " + " " + numero1 + " " + "mas" + " " + numero2 + " " + "es:" + " " + (numero1 + numero2));

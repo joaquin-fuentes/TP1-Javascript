d91cf9ec6b838a4b4788c4baf7d9101fb738d174
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-02-27 09:59:27 -0300

    Se agregó info al README y se practico otras maneras en ejercicio 05

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TP1-Javascript
+
 En este repositorio se encuentra el TP1 de Javascript (Modulo 2 del curso de RollingCode)
+
+# Conceptos básicos de javascript
+
+- Variables (var, let y const)
+
+- Tipos de datos (string, number, boolean, null, undefined, array, objet )
+
--- js/ejercicio05.js
+++ js/ejercicio05.js
@@ -2,6 +2,7 @@
 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
 */
 
-let numero1 = parseInt(window.prompt("Introduza un número: ")); 
-let numero2 = parseInt(window.prompt("Introduza otro número: ")); 
+let numero1 = parseInt(prompt("Introduza un número: ")); 
+let numero2 = parseInt(prompt("Introduza otro número: ")); 
 document.write("El resultado de sumar " + " " + numero1 + " " + "mas" + " " + numero2 + " " + "es:" + " " + (numero1 + numero2));
+document.write(`<br> El resultado de sumar ${numero1} + ${numero2} es = ${(numero1 + numero2)} `);
